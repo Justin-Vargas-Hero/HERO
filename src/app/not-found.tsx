40ba78d657b51989043a61773c24468ad4bc9be6
Justin-Vargas-Hero <justin@hero.us.org>
2025-10-09 01:31:47 -0400

    Fix 404 page

---
+++ src/app/not-found.tsx
@@ -0,0 +1,8 @@
+export default function NotFound() {
+    return (
+        <div className="flex flex-col items-center justify-center min-h-[calc(100vh-56px-56px)]">
+            <h1 className="text-6xl font-bold mb-4">404</h1>
+            <p className="text-lg text-gray-600">This page could not be found.</p>
+        </div>
+    );
+}
